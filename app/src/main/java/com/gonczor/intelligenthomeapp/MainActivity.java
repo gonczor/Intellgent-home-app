63c5c9a086f79a41281f35fbfdfad34a8f7b767f
efem15 <efem15@wp.pl>
2017-01-02 21:53:19 +0100

    changes in main activity + changed overall layout + added first widgets in layout file

--- app/src/main/java/com/gonczor/intelligenthomeapp/MainActivity.java
+++ app/src/main/java/com/gonczor/intelligenthomeapp/MainActivity.java
@@ -1,14 +1,25 @@
 // first commit Efem15
 package com.gonczor.intelligenthomeapp;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onNextClick(View view) {
+
+
     }
 }
